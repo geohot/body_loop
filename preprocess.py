47ef98cbc8d78f6d1b7c13bd8846249160c093b5
Juba1806 <elaissaouimohammed01@gmail.com>
2023-10-31 18:03:33 +0100

    Improve preprocessing function and code structure

--- preprocess.py
+++ preprocess.py
@@ -1,40 +1,54 @@
 #!/usr/bin/env python3
 import os
+import numpy as np
+from tqdm import tqdm
 from tinygrad.tensor import Tensor
 from tinygrad.nn.state import safe_save
 from control import get_foundation
 from video import frames_from_file
-from tinygrad.tensor import Tensor
+
 Tensor.training = False
 Tensor.no_grad = True
-import numpy as np
-from tqdm import tqdm
+
+IMAGE_HEIGHT = 480
+IMAGE_WIDTH = 640
+NUM_CHANNELS = 3
 
 def preprocess(foundation, fn, fn_out, flip=False):
-  if not fn.endswith(".hevc"): return
-  if 'online' in fn: return
-  print("preprocessing", fn, fn_out)
-  if os.path.isfile(fn_out): return
-  frms = []
-  xs = []
-  for frm in tqdm(frames_from_file(fn)):
-    frms.append(frm)
-    if len(frms) == 256:
-      data = Tensor(np.concatenate([x.reshape(1,480,640,3) for x in frms]))
-      if flip: data = data[:, :, ::-1]
-      xs.append(foundation(data).realize())
-      frms = []
-  if frms:
-    data = Tensor(np.concatenate([x.reshape(1,480,640,3) for x in frms]))
-    if flip: data = data[:, :, ::-1]
-    xs.append(foundation(data).realize())
-  big_x = Tensor.cat(*xs, dim=0)
-  print(big_x.shape)
-  return safe_save({"x": big_x}, fn_out)
+    """
+    Preprocesses video frames and saves the result to a file.
+
+    Args:
+        foundation (callable): The foundation function.
+        fn (str): Input video file path.
+        fn_out (str): Output file path.
+        flip (bool, optional): Whether to flip the frames. Defaults to False.
+    """
+    if not fn.endswith(".hevc"):
+        return
+    if 'online' in fn:
+        return
+    print("Preprocessing", fn, fn_out)
+    if os.path.isfile(fn_out):
+        return
+    
+    frms = [frm for frm in frames_from_file(fn)]
+    xs = [foundation(Tensor(np.concatenate([x.reshape(1, IMAGE_HEIGHT, IMAGE_WIDTH, NUM_CHANNELS) for x in frms])))
+          .realize() for frms in np.array_split(frms, len(frms) // 256)]
+    
+    big_x = Tensor.cat(*xs, dim=0)
+    print(big_x.shape)
+    safe_save({"x": big_x}, fn_out)
+
+def main():
+    foundation = get_foundation()
+    data_dir = "data"
+    
+    for fn in os.listdir(data_dir):
+        if fn.endswith(".hevc"):
+            preprocess(foundation, os.path.join(data_dir, fn), os.path.join(data_dir, fn.replace(".hevc", ".safetensors")))
+            preprocess(foundation, os.path.join(data_dir, fn), os.path.join(data_dir, fn.replace(".hevc", "_flip.safetensors")), True)
 
 if __name__ == "__main__":
-  foundation = get_foundation()
-  for fn in os.listdir("data"):
-    preprocess(foundation, "data/"+fn, "data/"+fn.replace(".hevc", ".safetensors"))
-    preprocess(foundation, "data/"+fn, "data/"+fn.replace(".hevc", "_flip.safetensors"), True)
+    main()
 
